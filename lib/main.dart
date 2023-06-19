c55a40a0183645c93c14dfc4414509ab6f4c45f8
don2dusk <otegabrotobor@gmail.com>
2023-06-19 23:31:08 +0100

    initialCommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'ffi.dart' if (dart.library.html) 'ffi_web.dart';
+import 'ffi.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Octernships Project',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Elevate priviledge to run a Linux command'),
+      home:
+          const MyHomePage(title: 'Elevate priviledge to run a Linux command'),
     );
   }
 }
@@ -29,21 +31,135 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String username = "";
+  String dir = "";
+
+  Future<void> getUser() async {
+    await api.getUsername().then((value) {
+      setState(() {
+        username = value;
+      });
+    });
+  }
+
+  Future<void> returnFolder(String password) async {
+    await api.printHomeFolder(password: password).then((value) {
+      setState(() {
+        dir = value!;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text("ls -la /root/", style: TextStyle(fontSize: 40.0)),
-            const Text('Run above cmd with Rust and print the output here'),
-          ],
-        ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  passwordDialog();
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.lightBlue,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 35),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                child: const Text(
+                  "Show my Home Folder",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600, color: Colors.white),
+                )),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            dir,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                fontSize: 16, fontWeight: FontWeight.w400, color: Colors.black),
+          )
+        ],
       ),
     );
+  }
+
+  void passwordDialog() {
+    showDialog<void>(
+        context: context,
+        builder: (BuildContext dialogContext) {
+          String password = "";
+          return AlertDialog(
+            backgroundColor: const Color(0xFFE0E0E0),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            content:
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text("[sudo] enter password for $username: "),
+              const SizedBox(width: 10),
+              SizedBox(
+                width: 100,
+                height: 40,
+                child: TextField(
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  obscureText: true,
+                ),
+              )
+            ]),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("Back"),
+              ),
+              TextButton(
+                onPressed: () async {
+                  setState(() {
+                    dir = "";
+                  });
+                  await returnFolder(password).then((value) {
+                    if (dir.isEmpty) {
+                      final snackbar = SnackBar(
+                        backgroundColor: Colors.red,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(15),
+                                topRight: Radius.circular(15))),
+                        content: const Text(
+                          "Incorrect Password, Please try again",
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        action:
+                            SnackBarAction(label: 'Close', onPressed: () {}),
+                      );
+
+                      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                    } else {
+                      Navigator.pop(context);
+                    }
+                  });
+                },
+                child: const Text("Next"),
+              )
+            ],
+          );
+        });
   }
 }
--- native/src/api.rs
+++ native/src/api.rs
@@ -1,59 +1,33 @@
-// This is the entry point of your Rust library.
-// When adding new code to your project, note that only items used
-// here will be transformed to their Dart equivalents.
+use std::{process::Command, io::Write};
 
-// A plain enum without any fields. This is similar to Dart- or C-style enums.
-// flutter_rust_bridge is capable of generating code for enums with fields
-// (@freezed classes in Dart and tagged unions in C).
-pub enum Platform {
-    Unknown,
-    Android,
-    Ios,
-    Windows,
-    Unix,
-    MacIntel,
-    MacApple,
-    Wasm,
+pub fn get_username() -> String {
+    let process = Command::new("whoami").output().expect("failed to execute");
+    let username = String::from_utf8(process.stdout)
+        .expect("failed to convert to string")
+        .trim()
+        .to_string();
+
+    return username;
 }
 
-// A function definition in Rust. Similar to Dart, the return type must always be named
-// and is never inferred.
-pub fn platform() -> Platform {
-    // This is a macro, a special expression that expands into code. In Rust, all macros
-    // end with an exclamation mark and can be invoked with all kinds of brackets (parentheses,
-    // brackets and curly braces). However, certain conventions exist, for example the
-    // vector macro is almost always invoked as vec![..].
-    //
-    // The cfg!() macro returns a boolean value based on the current compiler configuration.
-    // When attached to expressions (#[cfg(..)] form), they show or hide the expression at compile time.
-    // Here, however, they evaluate to runtime values, which may or may not be optimized out
-    // by the compiler. A variety of configurations are demonstrated here which cover most of
-    // the modern oeprating systems. Try running the Flutter application on different machines
-    // and see if it matches your expected OS.
-    //
-    // Furthermore, in Rust, the last expression in a function is the return value and does
-    // not have the trailing semicolon. This entire if-else chain forms a single expression.
-    if cfg!(windows) {
-        Platform::Windows
-    } else if cfg!(target_os = "android") {
-        Platform::Android
-    } else if cfg!(target_os = "ios") {
-        Platform::Ios
-    } else if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
-        Platform::MacApple
-    } else if cfg!(target_os = "macos") {
-        Platform::MacIntel
-    } else if cfg!(target_family = "wasm") {
-        Platform::Wasm
-    } else if cfg!(unix) {
-        Platform::Unix
+pub fn print_home_folder(password : String) -> Option<String> {
+    let mut command = Command::new("sudo")
+        .args(["-k","-S", "ls", "-la", "/root/"])
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("failed to execute command");
+
+    let mut input = command.stdin.take().expect("failed to open stdin");
+    std::thread::spawn(move || {input.write_all(password.as_bytes()).expect("failed to write to stdin")});
+
+    let output = command
+        .wait_with_output()
+        .expect("failed to wait for child process");
+
+    if output.status.code() == Some(0) {
+        return Some(String::from_utf8(output.stdout).expect("failed to convert to string"));
     } else {
-        Platform::Unknown
+        return None;
     }
-}
-
-// The convention for Rust identifiers is the snake_case,
-// and they are automatically converted to camelCase on the Dart side.
-pub fn rust_release_mode() -> bool {
-    cfg!(not(debug_assertions))
 }
