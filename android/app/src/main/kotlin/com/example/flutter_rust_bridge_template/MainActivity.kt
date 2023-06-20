5b8dce2d1f97424dfd3b34f293941fb5df6f99e2
Manglam <64706997+Mangalofficial@users.noreply.github.com>
2023-06-20 22:12:26 +0530

    Updated MainActivity.kt

--- android/app/src/main/kotlin/com/example/flutter_rust_bridge_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_rust_bridge_template/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.example.flutter_rust_bridge_template
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+        private val CHANNEL = "manglam"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+                .setMethodCallHandler { call, result ->
+                    if (call.method == "executePolkitAction") {
+                        // Implement your Polkit action execution here
+                        // Return 'success' if Polkit action succeeds, otherwise return 'failure'
+                        result.success("success")
+                    } else {
+                        result.notImplemented()
+                    }
+                }
+    }
 }
